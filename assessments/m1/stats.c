0ac88b99b725d27a4cff0a5bc788eae6f5cf2d73
Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
2026-08-12 12:16:24 +0300

    Squashed commit of the following:
    commit ffb6c93cd811c82a5eb31583354ee6295de4a01b
    Author: Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
    Date:   Fri Jul 24 14:51:16 2026 +0300
    
        Update README.md
    
    commit ff1265895b6dc8fbee229828af9b2655c779bb0d
    Author: Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
    Date:   Fri Jul 24 14:45:24 2026 +0300
    
        Final Version
    
        Finished Documentations and Fixed Errors
    
    commit 3b440a813a3e236cc6b512d4e866b58f8a8bae38
    Author: Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
    Date:   Fri Jul 24 13:08:21 2026 +0300
    
        Finished Implementation
    
        Documentation Remaining
    
    commit e4fdd0fa5b99a48c81e51d5eea966994a222f106
    Author: Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
    Date:   Fri Jul 24 12:49:10 2026 +0300
    
        Function Declarations & Started Implementation
    
    commit c5c86e9011c4100cb00322c873bd2ef7ff304e9f
    Author: Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
    Date:   Fri Jul 24 10:38:20 2026 +0300
    
        ignore
    
    commit a107954b3e35136aeeb4068c52bdbfcbdbdd82f9
    Author: Mostafa Elshorbagy <mostafaelshorbagy2005@gmail.com>
    Date:   Fri Jul 24 10:23:09 2026 +0300
    
        Setup
    
        Also did basic testing to see if everything functional

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -28,16 +28,99 @@
 #define SIZE (40)
 
 void main() {
-
+printf("testing111\n");
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+print_array(test, SIZE);
+printf("\n***********************************************\n");
+sort_array(test, SIZE);
+print_array(test, SIZE);
+printf("\n***********************************************\n");
+print_statistics(test, SIZE);
+
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
 }
+void print_array (unsigned char arr[], int size) {
+  printf("[");
+  for(int i=0; i<size; i++){
+    if(i == size-1){
+      printf("%d]",arr[i]);
+      break;
+    }
+    else{
+    printf("%d, ",arr[i]);
+    }
+  }
+}
 
 /* Add other Implementation File Code Here */
+void sort_array(unsigned char arr[], int size){
+  int temp;
+  for(int i=0;i<size;i++){
+    int champ=i;
+      for(int j=i;j<size;j++){
+        if(arr[champ]<arr[j]){
+          champ=j;
+        }
+      }
+      temp = arr[i];
+      arr[i] = arr[champ];
+      arr[champ] = temp;
+  }
+}
+
+unsigned char find_minimum (unsigned char arr[], int size){
+  int champ = 0;
+  for(int i=1;i<size;i++){
+    if (arr[champ]> arr[i]){
+      champ = i;
+    }
+  }
+  return arr[champ];
+}
+unsigned char find_maximum (unsigned char arr[], int size){
+  int champ = 0;
+  for(int i=1;i<size;i++){
+    if (arr[champ]< arr[i]){
+      champ = i;
+    }
+  }
+  return arr[champ];
+}
+unsigned char find_mean (unsigned char arr[], int size){
+  int sum = 0;
+  for(int i=0;i<size;i++){
+    sum+= arr[i];
+  }
+  return sum/size;
+}
+
+unsigned char find_median (unsigned char arr[], int size){
+  sort_array(arr, size);
+  if (size%2==0){
+    char M1 = arr[size/2];
+    char M2 = arr[size/2 +1];
+    return (M1 + M2)/2;
+  }
+  else{
+    return arr[size/2];
+  }
+}
+
+ void print_statistics (unsigned char arr[], int size){
+
+printf("Max:%d",find_maximum(arr, size));
+printf("\n***********************************************\n");
+printf("Min:%d",find_minimum(arr, size));
+printf("\n***********************************************\n");
+printf("Mean:%d",find_mean(arr, size));
+printf("\n***********************************************\n");
+printf("Median:%d",find_median(arr, size));
+ }
+
